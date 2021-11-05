3b35c5ea85805f0e0a8191765004194b1eb91df4
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2021-11-05 12:25:13 +0100

    work work

--- test/Main.purs
+++ test/Main.purs
@@ -15,77 +15,67 @@ import Gun (toJSON)
 import Gun as Gun
 import Gun.Configuration (Configuration, fileOption, webOption)
 import Gun.Node (Saveable(..))
---import Gun.Query (Query, emptyQuery, from, map, on, once, onceExec, query)
+import Gun.Query (Query, emptyQuery, from, map, on, once, onceExec, query)
 import Gun.Query.Mapper (Mapper(..))
 
--- type RootData
---   = { state :: String }
+type RootData
+  = { state :: String }
 
--- rootDataToJson :: RootData -> Json
--- rootDataToJson = encodeJson
+rootDataToJson :: RootData -> Json
+rootDataToJson = encodeJson
 
--- rootDataFromJson :: Json -> Either JsonDecodeError RootData
--- rootDataFromJson = decodeJson
+rootDataFromJson :: Json -> Either JsonDecodeError RootData
+rootDataFromJson = decodeJson
 
--- type UserData
---   = { secret :: String }
+type UserData
+  = { secret :: String }
 
--- userDataToJson :: UserData -> Json
--- userDataToJson = encodeJson
+userDataToJson :: UserData -> Json
+userDataToJson = encodeJson
 
--- userDataFromJson :: Json -> Either JsonDecodeError UserData
--- userDataFromJson = decodeJson
+userDataFromJson :: Json -> Either JsonDecodeError UserData
+userDataFromJson = decodeJson
 
--- -- userDataQuery :: forall a. Query ( secret :: String ) 
--- -- userDataQuery =
--- --   emptyQuery
--- --     # from "user"
--- --     # from "data"
--- --     # map Passthrough
--- --     # onceExec \d -> do
--- --       jsondata <- (toJSON d)
--- --       case userDataFromJson jsondata of 
--- --         (Left error) ->
--- --           pure $ trace {error} identity
--- --         (Right data') -> 
--- --           pure $ trace {data'} identity
--- --       pure unit
+userDataQuery :: forall a. Query ( secret :: String ) 
+userDataQuery =
+  emptyQuery
+    # from "user"
+    # from "data"
+    # map Passthrough
+    # onceExec \key d -> do
+      jsondata <- (toJSON d)
+      case userDataFromJson jsondata of 
+        (Left error) ->
+          pure $ trace {error} identity
+        (Right data') -> 
+          pure $ trace {data'} identity
+      pure unit
 
--- -- publicDataQuery :: forall a. Query a
--- -- publicDataQuery =
--- --   emptyQuery
--- --     # from "public"
--- --     # from "data"
--- --     # once
--- --     # on \d -> do
--- --       jsondata <- (toJSON d) 
--- --       pure $ trace jsondata identity
+main :: Effect Unit
+main =
+  launchAff_ do
 
--- main :: Effect Unit
--- main =
---   launchAff_ do
+    let
+      gunConfig :: Options Configuration
+      gunConfig =
+        webOption := Nothing
+          <> fileOption
+          := Just "radata"
 
---     let
---       gunConfig :: Options Configuration
---       gunConfig =
---         webOption := Nothing
---           <> fileOption
---           := Just "radata"
+    let gun = (Gun.create gunConfig)
+    let publicNode = Gun.get "public" gun
+    let publicDataNode = Gun.get "data" publicNode
+    let userNode = Gun.get "user" gun
+    let userDataNode = Gun.get "data" userNode
 
---     gun <- liftEffect $ (Gun.create gunConfig)
---     publicNode <- liftEffect $ Gun.get "public" gun
---     publicDataNode <- liftEffect $ Gun.get "data" publicNode
---     userNode <- liftEffect $ Gun.get "user" gun
---     userDataNode <- liftEffect $ Gun.get "data" userNode
+    _ <-
+      liftEffect
+        $ setInterval 500 do
+            let _ = userDataNode # Gun.put (SaveableRecord { secret: "some secrets" })
+            let _ = publicDataNode # Gun.put (SaveableRecord { publicdata: "some public data" })
+            pure unit
 
---     _ <-
---       liftEffect
---         $ setInterval 500 do
---             _ <- userDataNode # Gun.put (SaveableRecord { secret: "some secrets" })
---             _ <- publicDataNode # Gun.put (SaveableRecord { publicdata: "some public data" })
---             pure unit
-
---     _ <- liftEffect $ query userDataQuery gun
+    let _ = query userDataQuery gun
     -- _ <- liftEffect $ query publicDataQuery gun
     -- usernode <- liftEffect $ User.user gun
     -- bob <- User.createUser "bob" "bobspass" usernode
@@ -104,4 +94,4 @@ import Gun.Query.Mapper (Mapper(..))
     --     pure $ trace "exit" identity
     --   (User.AuthError { err }) -> do
     --     pure $ trace err identity
-    -- pure unit
+    pure unit
